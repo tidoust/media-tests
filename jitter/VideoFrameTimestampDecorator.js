6746fc1a8691ab22d31a05c8248a7d362decd654
Francois Daoust <fd@tidoust.net>
2022-11-03 10:29:53 +0100

    Make decoration code a TransformStream and add comments
    The code that decorates a VideoFrame with its color-coded timestamp in the
    bottom-right corner gets moved to its own class and file.
    
    The code is now full of comments to details the processing steps, and try
    to explain why things are the way they are.

---
+++ jitter/VideoFrameTimestampDecorator.js
@@ -0,0 +1,329 @@
+'use strict';
+
+importScripts('instrumented-transformstream.js');
+
+/**
+ * A type of TransformStream that can process a stream of VideoFrames to
+ * overlay its timestamp (in milliseconds) using a color code in the bottom
+ * right corner of the frame.
+ */
+class VideoFrameTimestampDecorator extends InstrumentedTransformStream {
+  /**
+   * Constructor takes config object, basically only needed to get the list of
+   * colors to use to encode the timestamp.
+   */
+  constructor(config) {
+    // Helper function to convert an hexadecimal color into 4 color components
+    // (note GPUs love float numbers between 0.0 and 1.0, including for colors)
+    function rgbToBytes(rgb) {
+      return [
+        parseInt(rgb.slice(1,3), 16), // Red component
+        parseInt(rgb.slice(3,5), 16), // Green component
+        parseInt(rgb.slice(5,7), 16), // Blue component
+        255                           // Alpha component
+      ].map(c => c / 255);            // Convert to floats from 0.0 to 1.0
+    }
+
+    // Internal variables used to set things up for WebGPU and keep track of
+    // the setup so that the "transform" function can use it.
+    let gpuDevice;
+    let fragmentBindGroupLayout;
+    let gpuPipeline;
+    let textureSampler;
+
+    // Convert hexadecimal rgb colors to GPU-friendly colors once and for all
+    const colorBytes = config.colors.map(rgbToBytes).flat();
+
+    // Create canvas onto which we'll render
+    const canvas = new OffscreenCanvas(
+      config?.width ?? 1920,
+      config?.height ?? 1080
+    );
+    const gpuCanvasContext = canvas.getContext('webgpu');
+
+    super({
+      /**
+       * Initialize the WebGPU context
+       */
+      async start(controller) {
+        // Initialize WebGPU and the canvas that we'll draw to.
+        // This is boilerplate code with default parameters.
+        const adapter = await navigator.gpu.requestAdapter();
+        gpuDevice = await adapter.requestDevice();
+        const format = navigator.gpu.getPreferredCanvasFormat();
+        gpuCanvasContext.configure({
+          device: gpuDevice,
+          format: format,
+          alphaMode: 'opaque',
+        });
+
+        // Not quite sure why the bind group layout needs to be created
+        // explicitly, as opposed to using the implicit one returned by
+        // "getBindGroupLayout" but that seems needed for the buffer param (the
+        // layout returned by "getBindGroupLayout" has 4 bindings instead of 3).
+        // Implicit vs. explicit seems to be a common source of confusion for
+        // beginners, see: https://github.com/gpuweb/gpuweb/issues/2470
+        fragmentBindGroupLayout = gpuDevice.createBindGroupLayout({
+          entries: [
+            { binding: 0, visibility: GPUShaderStage.FRAGMENT, sampler: {} },
+            { binding: 1, visibility: GPUShaderStage.FRAGMENT, externalTexture: {} },
+            { binding: 2, visibility: GPUShaderStage.FRAGMENT, buffer: { type: 'uniform' } },
+          ]
+        })
+
+        // Define the GPU render pipeline which, as all render pipelines,
+        // consists of a vertex shader that returns a list of triangles (2
+        // triangles that cover the whole canvas in our case) and a fragment
+        // shader that computes the color of each point in these triangles.
+        // The WGSL source of the shaders is at the end of this file.
+        gpuPipeline = gpuDevice.createRenderPipeline({
+          layout: gpuDevice.createPipelineLayout({
+            bindGroupLayouts: [
+              fragmentBindGroupLayout
+            ]
+          }),
+          vertex: {
+            module: gpuDevice.createShaderModule({
+              code: VideoFrameTimestampDecorator.#vertexShaderSource
+            }),
+            entryPoint: 'vert_main'
+          },
+          fragment: {
+            module: gpuDevice.createShaderModule({
+              code: VideoFrameTimestampDecorator.#fragmentShaderSource
+            }),
+            entryPoint: 'frag_main',
+            targets: [
+              { format: format }
+            ]
+          },
+          primitive: {
+            topology: 'triangle-list'
+          }
+        });
+
+        // Use default sampler configuration to sample the texture and get the
+        // color to use to render a particular pixel from it.
+        textureSampler = gpuDevice.createSampler({});
+      },
+
+
+      /**
+       * Process a new frame on the GPU to overlay the timestamp on top of the
+       * frame, and return a new VideoFrame with the result.
+       * 
+       * To process the new frame, parameters need to be sent to the GPU through
+       * a GPUBuffer. That is easier said than done. As apparently everyone
+       * should know, the offset of a struct member of type
+       * "array<vec4<u32>, 64>" in address space "uniform" must be a multiple of
+       * 16 bytes. The first two parameters in the structure take only 8 bytes,
+       * so we need to leave 8 additional bytes before we can send the colors.
+       * No error would be raised if we fail to do that (except if GPUBuffer
+       * size is not large enough) but colors wouldn't be the right ones, since
+       * RGBA components would be shifted by 2.
+       */
+      transform(frame, controller) {
+        // Adjust the size of the canvas to the size of the frame to process
+        // (In our case, all frames should have the same size so this is not
+        // really needed)
+        canvas.width = frame.displayWidth;
+        canvas.height = frame.displayHeight;
+
+        // Save the frame's timestamp
+        const timestamp = frame.timestamp;
+
+        // Prepare the GPUBuffer that will contain the parameters sent to the
+        // GPU (note the additional 8 bytes, and the offset at which colorBytes
+        // gets written).
+        // Note we can only encode reasonable numbers so timestamp gets
+        // converted to milliseconds.
+        const paramsBuffer = gpuDevice.createBuffer({
+          size: 2 * 4 + 8 + 64 * 4 * 4,
+          usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+          mappedAtCreation: true
+        });
+        const map = new Float32Array(paramsBuffer.getMappedRange());
+        map[0] = Math.floor(timestamp / 1000);
+        map[1] = config.colors.length;
+        map.set(colorBytes, 4);
+        paramsBuffer.unmap();
+
+        // Create the binding group with the sample, the texture and the params
+        const uniformBindGroup = gpuDevice.createBindGroup({
+          layout: fragmentBindGroupLayout,
+          entries: [
+            { binding: 0, resource: textureSampler },
+            { binding: 1, resource: gpuDevice.importExternalTexture({source: frame}) },
+            { binding: 2, resource: { buffer: paramsBuffer } }
+          ],
+        });
+
+        // The rest is pretty much boilerplate to prepare, queue and run draw
+        // commands on the GPU. 6 draws are needed, 3 per triangle.
+        // (Note the draw commands are for the vertex shader, the fragment
+        // shader gets called with interpolated coordinates for each pixel
+        // in the triangles, in other words ore than a million of times per
+        // frame).
+        const commandEncoder = gpuDevice.createCommandEncoder();
+        const textureView = gpuCanvasContext.getCurrentTexture().createView();
+        const renderPassDescriptor = {
+          colorAttachments: [
+            {
+              view: textureView,
+              clearValue: [1.0, 0.0, 0.0, 1.0],
+              loadOp: 'clear',
+              storeOp: 'store',
+            },
+          ],
+        };
+
+        const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+        passEncoder.setPipeline(gpuPipeline);
+        passEncoder.setBindGroup(0, uniformBindGroup);
+        passEncoder.draw(6, 1, 0, 0);
+        passEncoder.end();
+        gpuDevice.queue.submit([commandEncoder.finish()]);
+
+        // The frame was sent to the GPU as external texture, no need to keep the
+        // VideoFrame object open any longer in this CPU worker.
+        frame.close();
+
+        // Wait for GPU processing to be done, create and enqueue a VideoFrame
+        // out of the the resulting canvas and compute the overall time spent
+        // in the TransformStream.
+        return gpuDevice.queue
+          .onSubmittedWorkDone()
+          .then(_ => {
+            const frame = new VideoFrame(canvas, {
+              timestamp: timestamp,
+              alpha: 'discard'
+            });
+            this.setEndTime(timestamp);
+            controller.enqueue(frame);
+          });
+      }
+    });
+  }
+
+
+  /**
+   * Vertex shader:
+   * Receives the vertex index as parameter, from 0 to 5. Vertices 0, 1, 2
+   * create the bottom-right triangle. Vertices 3, 4, 5 create the top-left
+   * triangle. The two triangles cover the whole canvas.
+   *
+   * Note uv coordinates are from 0.0 to 1.0, from top left to bottom right,
+   * whereas pos coordinates follow the clip space from -1.0 to 1.0, from
+   * bottom left to top right. The uv coordinates are more convenient to deal
+   * with because they match those used in video frames.
+   */
+  static #vertexShaderSource = `
+    struct VertexOutput {
+      @builtin(position) Position: vec4<f32>,
+      @location(0) uv: vec2<f32>,
+    }
+
+    @vertex
+    fn vert_main(@builtin(vertex_index) VertexIndex: u32) -> VertexOutput {
+      var pos = array<vec2<f32>, 6>(
+        vec2<f32>( 1.0,  1.0),
+        vec2<f32>( 1.0, -1.0),
+        vec2<f32>(-1.0, -1.0),
+        vec2<f32>( 1.0,  1.0),
+        vec2<f32>(-1.0, -1.0),
+        vec2<f32>(-1.0,  1.0)
+      );
+
+      var uv = array<vec2<f32>, 6>(
+        vec2<f32>(1.0, 0.0),
+        vec2<f32>(1.0, 1.0),
+        vec2<f32>(0.0, 1.0),
+        vec2<f32>(1.0, 0.0),
+        vec2<f32>(0.0, 1.0),
+        vec2<f32>(0.0, 0.0)
+      );
+
+      var output : VertexOutput;
+      output.Position = vec4<f32>(pos[VertexIndex], 0.0, 1.0);
+      output.uv = uv[VertexIndex];
+      return output;
+    }
+  `;
+
+
+  /**
+   * Fragment shader:
+   * Receives the uv coordinates of the pixel to render as parameter.
+   * Expects a sampler, used to get pixels out of a texture, an external texture
+   * that represents the frame to draw, and a parameters structure with the
+   * timestamp to render as overlay, along with the colors to use, to be set in
+   * what GPU specs call a binding group.
+   * The parameters structure is a "uniform" because the variable is to hold
+   * the same value for all calls of the fragment shader.
+   * 
+   * The shader returns the color of the pixel to render, which is either the
+   * color of the corresponding pixel in the video frame, or a color that
+   * encodes one of the digits of the frame's timestamp (in base "number of
+   * colors") when the pixel is in the bottom right corner of the canvas.
+   */
+  static #fragmentShaderSource = `
+    struct Params {
+      timestamp: f32,
+      nbColors: f32,
+      colors: array<vec4<f32>,64>
+    }
+
+    @group(0) @binding(0) var mySampler: sampler;
+    @group(0) @binding(1) var myTexture: texture_external;
+    @group(0) @binding(2) var<uniform> params: Params;
+
+    // Helper function that converts a number to 4 digits in the given base
+    fn nbToDigits(nb: f32, base: f32) -> vec4<u32> {
+      let first: u32 = u32(nb % base);
+      let firstremainder: f32 = trunc(nb / base);
+      let second: u32 = u32(firstremainder % base);
+      let secondremainder: f32 = trunc(firstremainder / base);
+      let third: u32 = u32(secondremainder % base);
+      let thirdremainder: f32 = trunc(secondremainder / base);
+      let fourth: u32 = u32(thirdremainder % base);
+      return vec4<u32>(fourth, third, second, first);
+    }
+
+    // Helper function that returns the color representing the digit
+    // (in base "number of colors") of the timestamp at the given
+    // index (from 0 to 3).
+    fn timestampToColor(ts: f32, index: u32) -> vec4<f32> {
+      let digits: vec4<u32> = nbToDigits(ts, params.nbColors);
+      let digit: u32 = digits[index];
+      let color: vec4<f32> = params.colors[digit];
+      return vec4<f32>(color[0], color[1], color[2], 1.0);
+    }
+
+    // The main function of the fragment shader
+    // TODO: Re-write to avoid "if... then... else..."
+    @fragment
+    fn frag_main(@location(0) uv : vec2<f32>) -> @location(0) vec4<f32> {
+      if (uv.x > 0.5 && uv.y > 0.5) {
+        if (uv.x > 0.75) {
+          if (uv.y > 0.75) {
+            return timestampToColor(params.timestamp, 3);
+          }
+          else {
+            return timestampToColor(params.timestamp, 1);
+          }
+        }
+        else {
+          if (uv.y > 0.75) {
+            return timestampToColor(params.timestamp, 2);
+          }
+          else {
+            return timestampToColor(params.timestamp, 0);
+          }
+        }
+      }
+      else {
+        return textureSampleLevel(myTexture, mySampler, uv);
+      }
+    }
+  `;
+}
--- jitter/worker-wgpu-inputstream.js
+++ jitter/worker-wgpu-inputstream.js
@@ -1,249 +1,21 @@
 'use strict';
 
+importScripts('VideoFrameTimestampDecorator.js');
+
 // TEMP: workaround Chrome failure to close VideoFrames in workers
 // when they are transferred to the main thread.
 // Drop whenever possible!
 const framesToClose = {};
 
-function rgbToBytes(rgb) {
-  return [
-    parseInt(rgb.slice(1,3), 16),
-    parseInt(rgb.slice(3,5), 16),
-    parseInt(rgb.slice(5,7), 16),
-    255
-  ].map(c => c / 255);
-}
-
-// Generates two triangles covering the whole canvas.
-const vertexShaderSource = `
-  struct VertexOutput {
-    @builtin(position) Position: vec4<f32>,
-    @location(0) uv: vec2<f32>,
-  }
-
-  @vertex
-  fn vert_main(@builtin(vertex_index) VertexIndex: u32) -> VertexOutput {
-    var pos = array<vec2<f32>, 6>(
-      vec2<f32>( 1.0,  1.0),
-      vec2<f32>( 1.0, -1.0),
-      vec2<f32>(-1.0, -1.0),
-      vec2<f32>( 1.0,  1.0),
-      vec2<f32>(-1.0, -1.0),
-      vec2<f32>(-1.0,  1.0)
-    );
-
-    var uv = array<vec2<f32>, 6>(
-      vec2<f32>(1.0, 0.0),
-      vec2<f32>(1.0, 1.0),
-      vec2<f32>(0.0, 1.0),
-      vec2<f32>(1.0, 0.0),
-      vec2<f32>(0.0, 1.0),
-      vec2<f32>(0.0, 0.0)
-    );
-
-    var output : VertexOutput;
-    output.Position = vec4<f32>(pos[VertexIndex], 0.0, 1.0);
-    output.uv = uv[VertexIndex];
-    return output;
-  }
-`;
-
-// Sample the external texture using generated coordinates
-// and overlay the timestamp in the bottom right corner
-const fragmentShaderSource = `
-  struct Params {
-    timestamp: f32,
-    nbColors: f32,
-    colors: array<vec4<f32>,64>
-  }
-
-  @group(0) @binding(0) var mySampler: sampler;
-  @group(0) @binding(1) var myTexture: texture_external;
-  @group(0) @binding(2) var<uniform> params: Params;
-
-  // Convert number to 4 digits in the given base
-  fn nbToDigits(nb: f32, base: f32) -> vec4<u32> {
-    let first: u32 = u32(nb % base);
-    let firstremainder: f32 = trunc(nb / base);
-    let second: u32 = u32(firstremainder % base);
-    let secondremainder: f32 = trunc(firstremainder / base);
-    let third: u32 = u32(secondremainder % base);
-    let thirdremainder: f32 = trunc(secondremainder / base);
-    let fourth: u32 = u32(thirdremainder % base);
-    return vec4<u32>(fourth, third, second, first);
-  }
-
-  fn timestampToColor(ts: f32, index: u32) -> vec4<f32> {
-    let digits: vec4<u32> = nbToDigits(ts, params.nbColors);
-    let digit: u32 = digits[index];
-    let color: vec4<f32> = params.colors[digit];
-    return vec4<f32>(color[0], color[1], color[2], 1.0);
-  }
-
-  @fragment
-  fn frag_main(@location(0) uv : vec2<f32>) -> @location(0) vec4<f32> {
-    // TODO: Re-write to avoid "if... then... else..."
-    if (uv.x > 0.5 && uv.y > 0.5) {
-      if (uv.x > 0.75) {
-        if (uv.y > 0.75) {
-          return timestampToColor(params.timestamp, 3);
-        }
-        else {
-          return timestampToColor(params.timestamp, 1);
-        }
-      }
-      else {
-        if (uv.y > 0.75) {
-          return timestampToColor(params.timestamp, 2);
-        }
-        else {
-          return timestampToColor(params.timestamp, 0);
-        }
-      }
-    }
-    else {
-      return textureSampleLevel(myTexture, mySampler, uv);
-    }
-  }
-`;
 
 self.addEventListener('message', async function(e) {
   if (e.data.type === 'start') {
+    VideoFrameTimestampDecorator.resetStats();
     const inputStream = e.data.streams.input;
     const outputStream = e.data.streams.output;
     const config = e.data.config;
-    const colorBytes = config.colors.map(rgbToBytes).flat();
 
-    const addTimestampToFrame = new TransformStream({
-      async start(controller) {
-        // Initialize WebGPU and the canvas that we'll draw to
-        const adapter = await navigator.gpu.requestAdapter();
-        this.device = await adapter.requestDevice();
-        this.format = navigator.gpu.getPreferredCanvasFormat();
-
-        this.canvas = new OffscreenCanvas(config.width, config.height);
-
-        this.ctx = this.canvas.getContext('webgpu');
-        this.ctx.configure({
-          device: this.device,
-          format: this.format,
-          alphaMode: 'opaque',
-        });
-
-        // The fragment shader takes a sampler, an external texture
-        // and a buffer that contains the timestamp and the colors to use
-        // to encode the timestamp.
-        // (Not quite sure why creating an explicit bind group layout is
-        // needed, as opposed to using the one returned by getBindGroupLayout
-        // but that's needed once a buffer input is used)
-        this.fragmentBindGroupLayout = this.device.createBindGroupLayout({
-          entries: [
-            { binding: 0, visibility: GPUShaderStage.FRAGMENT, sampler: {} },
-            { binding: 1, visibility: GPUShaderStage.FRAGMENT, externalTexture: {} },
-            { binding: 2, visibility: GPUShaderStage.FRAGMENT, buffer: { type: 'uniform' } },
-          ]
-        })
-
-        // Define the render pipeline which, as all render pipelines, consists
-        // of a vertex shader and a fragment shader. The vertex shader returns
-        // a list of triangles (in practice, 2 triangles that cover the entire
-        // canvas)
-        this.pipeline = this.device.createRenderPipeline({
-          layout: this.device.createPipelineLayout({
-            bindGroupLayouts: [ this.fragmentBindGroupLayout ]
-          }),
-          vertex: {
-            module: this.device.createShaderModule({code: vertexShaderSource}),
-            entryPoint: 'vert_main'
-          },
-          fragment: {
-            module: this.device.createShaderModule({code: fragmentShaderSource}),
-            entryPoint: 'frag_main',
-            targets: [{format: this.format}]
-          },
-          primitive: {
-            topology: 'triangle-list'
-          }
-        });
-
-        // Use default sampler configuration
-        this.sampler = this.device.createSampler({});
-      },
-
-      transform(frame, controller) {
-        this.canvas.width = frame.displayWidth;
-        this.canvas.height = frame.displayHeight;
-
-        // For each frame, we need to send a few parameters to the GPU:
-        // the frame's timestamp, the colors array to use to encode the
-        // timestamp, and then the frame itself as external structure
-        // along with the sampler to copy the pixels from the frame.
-        const timestamp = frame.timestamp;
-
-        // As apparently everyone should know, the offset of a struct
-        // member of type 'array<vec4<u32>, 64>' in address space
-        // 'uniform' must be a multiple of 16 bytes. This means we need
-        // to leave 8 bytes in the buffer after the first two parameters.
-        const paramsBuffer = this.device.createBuffer({
-          size: 2 * 4 + 8 + 64 * 4 * 4,
-          usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-          mappedAtCreation: true
-        });
-
-        // As noted above, map[2] and map[3] are left for alignment
-        const map = new Float32Array(paramsBuffer.getMappedRange());
-        map[0] = Math.floor(timestamp / 1000);
-        map[1] = config.colors.length;
-        map.set(colorBytes, 4);
-        paramsBuffer.unmap();
-
-        const uniformBindGroup = this.device.createBindGroup({
-          layout: this.fragmentBindGroupLayout,
-          entries: [
-            { binding: 0, resource: this.sampler },
-            { binding: 1, resource: this.device.importExternalTexture({source: frame}) },
-            { binding: 2, resource: { buffer: paramsBuffer } }
-          ],
-        });
-
-        // The rest is pretty much boilerplate to prepare, queue and run
-        // draw commands on the GPU. 6 draws are needed, 3 per triangle
-        // (note the draw commands are for the vertex shader, the fragment
-        // shader gets called with interpolated coordinates for each pixel)
-        const commandEncoder = this.device.createCommandEncoder();
-        const textureView = this.ctx.getCurrentTexture().createView();
-        const renderPassDescriptor = {
-          colorAttachments: [
-            {
-              view: textureView,
-              clearValue: [1.0, 0.0, 0.0, 1.0],
-              loadOp: 'clear',
-              storeOp: 'store',
-            },
-          ],
-        };
-
-        const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-        passEncoder.setPipeline(this.pipeline);
-        passEncoder.setBindGroup(0, uniformBindGroup);
-        passEncoder.draw(6, 1, 0, 0);
-        passEncoder.end();
-        this.device.queue.submit([commandEncoder.finish()]);
-
-        frame.close();
-
-        // We'll create a VideoFrame from the resulting canvas
-        return this.device.queue
-          .onSubmittedWorkDone()
-          .then(_ => {
-            const frame = new VideoFrame(this.canvas, {
-              timestamp: timestamp,
-              alpha: 'discard'
-            });
-            controller.enqueue(frame);
-          });
-      }
-    });
+    const addTimestampToFrame = new VideoFrameTimestampDecorator(config);
 
     inputStream
       .pipeThrough(addTimestampToFrame)
@@ -261,6 +33,9 @@ self.addEventListener('message', async function(e) {
       .pipeTo(outputStream);
   }
   else if (e.data.type === 'stop') {
+    const stats = VideoFrameTimestampDecorator.collectStats();
+    VideoFrameTimestampDecorator.resetStats();
+    self.postMessage({ type: 'stats', stats });
   }
   // TEMP: workaround Chrome failure to close VideoFrames in workers
   // when they are transferred to the main thread.
